8c987c7bbb6410aa9a03198e794be1755b0bcc33
Diego <76270550+Diego781@users.noreply.github.com>
2023-08-13 16:18:46 -0300

    Atualização
    Uma nova versão do projeto,trazendo algumas correções e melhorias.

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-function treino() {
-  let num = document.getElementById("txt1");
-  let res = document.getElementById("res");
-  let n = Number(num.value);
+function updatedSelect() {
+  let select = document.querySelector("#treinos");
+  let optionValue = select.options[select.selectedIndex];
+  let res = document.querySelector(".res");
+  let value = optionValue.value;
+  let text = optionValue.text;
 
-  if (num.value.length == 0) {
-    alert("Digite o número 1,2 ou 3");
-  } else if (num.value.length == 1) {
-    res.innerHTML = `<p>Divisão de Treino:</p>`;
-    res.innerHTML = `<p><strong>Treino 1 : Peito,triceps e ombro: </strong></p> 
-    <p><strong>Peito</strong> : Supino reto - supino inclinado - voador - cruxifixo; </p> 
+  console.log(value, text);
+
+  if (value === "Treino 1") {
+    res.innerHTML = `<strong>Peito,triceps e ombro</strong>: 
+    <p><strong>Peito</strong> : Supino reto - supino inclinado - voador - cruxifixo; </p>
     <p><strong>Ombro</strong> : Desenvolvimento - Elevação lateral - Elevação Frontal; </p>
-    <p><strong>Triceps</strong> :  Triceps corda - Triceps pulley - Triceps Testa; `;
-  }
-  if (num.value == 2) {
+    <p><strong>Triceps</strong> :  Triceps corda - Triceps pulley - Triceps Testa; </p>`;
+  } else if (value === "Treino 2") {
     res.innerHTML = `<p><strong>Treino 2 : Costas e Biceps</strong></p> 
     <p><strong>Costas</strong> : Puxada frontal - Remada unilateral - Remada Curvada - Pulldown; </p> 
     <p><strong>Biceps</strong> : Rosca direta - Rosca scott - Rosca direta com halteres no banco inclinado - Rosca martelo; </p>`;
-  } else if (num.value == 3) {
+  } else if (value === "Treino 3") {
     res.innerHTML = `<p><strong>Treino 3 : Quadriceps,posteriores e panturrilhas</strong></p> 
     <p><strong>Quadriceps</strong> : Agachamento livre - Leg Press 360 - Cadeira extensora - (4 repetições cada um); </p> 
     <p><strong>Posteriores</strong> : Stiff - Flexora sentada - Flexora deitada; </p>
     <p><strong>Panturilhas</strong> : Panturilhas na máquina sentado - Panturrilhas na máquina em pé;(5 repetições para cada exercício). </p>`;
   }
-  num.value = "";
-  num.focus();
 }
+// updatedSelect();
